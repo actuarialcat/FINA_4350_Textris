1c3c7fa680cbde59d0266b04cc2d72f4f664cbbd
mysunsun <73704107+mysunsun@users.noreply.github.com>
2020-11-22 23:42:17 +0800

    Revert "Create Product_Review_tfidf.py"
    This reverts commit e025ee9c6ed3ff29b75e6af3f459db6ee86734ac.

--- Product_Review_tfidf.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
-from nltk.stem import WordNetLemmatizer
-from nltk.util import ngrams
-from collections import defaultdict
-import re
-import datetime
-import os
