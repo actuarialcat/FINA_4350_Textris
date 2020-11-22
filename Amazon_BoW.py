9bbd9bfc7578c331770e51364eff1088e95a1d46
kchan825 <kevinwai825@gmail.com>
2020-11-22 20:41:31 +0800

    Amazon BoW

--- Amazon_BoW.py
+++ Amazon_BoW.py
@@ -20,14 +20,18 @@ for brand in brands:
     directory = "Product review data_{0}/".format(brand)
     review_files = os.listdir(directory)
 
-    result = pd.DataFrame(columns=["Month", 'Unigram Top 30', 'Bigram Top 20', 'Trigram Top 10'])
+    unigram_all_tokens = []
+    bigram_all_tokens = []
+    trigram_all_tokens = []
     unigram = {}
     bigram = {}
     trigram = {}
+    unigram_count = {}
+    bigram_count = {}
+    trigram_count = {}
 
     for review_file in review_files:
         data = pd.read_csv(directory + review_file)
-        print(review_file)
         title_total_polarity = []
         content_total_polarity = []
         for i in data.index:
@@ -50,28 +54,81 @@ for brand in brands:
             content_bigram_token = [' '.join(ng) for ng in ngrams(content_filtered_tokens, 2)]
             content_trigram_token = [' '.join(ng) for ng in ngrams(content_filtered_tokens, 3)]
 
+            unigram_all_tokens += title_lemmatized_tokens + content_lemmatized_tokens
+            bigram_all_tokens += title_bigram_token + content_bigram_token
+            trigram_all_tokens += title_trigram_token + content_trigram_token
+
             if index in unigram:
                 unigram[index] += title_lemmatized_tokens + content_lemmatized_tokens
                 bigram[index] += title_bigram_token + content_bigram_token
                 trigram[index] += title_trigram_token + content_trigram_token
+                unigram_count[index] += len(title_lemmatized_tokens + content_lemmatized_tokens)
+                bigram_count[index] += len(title_bigram_token + content_bigram_token)
+                trigram_count[index] += len(title_trigram_token + content_trigram_token)
             else:
                 unigram[index] = title_lemmatized_tokens + content_lemmatized_tokens
                 bigram[index] = title_bigram_token + content_bigram_token
                 trigram[index] = title_trigram_token + content_trigram_token
+                unigram_count[index] = len(title_lemmatized_tokens + content_lemmatized_tokens)
+                bigram_count[index] = len(title_bigram_token + content_bigram_token)
+                trigram_count[index] = len(title_trigram_token + content_trigram_token)
 
-    for key in sorted(unigram):
+    """
+    Finding top30/20/10 most used token of unigram/bigram/trigram 
+    """
+    unigram_target = defaultdict(int)
+    for tk in unigram_all_tokens:
+        unigram_target[tk] += 1
+    unigram_target = [key for key, value in sorted(unigram_target.items(), key=lambda kv: kv[1], reverse=True)[:30]]
+
+    bigram_target = defaultdict(int)
+    for tk in bigram_all_tokens:
+        bigram_target[tk] += 1
+    bigram_target = [key for key, value in sorted(bigram_target.items(), key=lambda kv: kv[1], reverse=True)[:20]]
+
+    trigram_target = defaultdict(int)
+    for tk in trigram_all_tokens:
+        trigram_target[tk] += 1
+    trigram_target = [key for key, value in sorted(trigram_target.items(), key=lambda kv: kv[1], reverse=True)[:10]]
+    columns = ["name"]+unigram_target+bigram_target+trigram_target
+    result = pd.DataFrame(columns=columns)
+
+    """
+    check the occurrence of most used token in each month  
+    """
+    for index in sorted(unigram):
         unigram_bow = defaultdict(int)
         bigram_bow = defaultdict(int)
         trigram_bow = defaultdict(int)
-        for tk in unigram[key]:
+        resultRow = [index]
+        for tk in unigram[index]:
             unigram_bow[tk] += 1
-        for tk in bigram[key]:
+        for tk in bigram[index]:
             bigram_bow[tk] += 1
-        for tk in trigram[key]:
+        for tk in trigram[index]:
             trigram_bow[tk] += 1
-        unigram_bow = sorted(unigram_bow.items(), key=lambda kv: kv[1], reverse=True)[:30]
-        bigram_bow = sorted(bigram_bow.items(), key=lambda kv: kv[1], reverse=True)[:20]
-        trigram_bow = sorted(trigram_bow.items(), key=lambda kv: kv[1], reverse=True)[:10]
-        result = result.append({'Month': key, 'Unigram Top 30': unigram_bow, 'Bigram Top 20': bigram_bow, 'Trigram Top 1': trigram_bow}, ignore_index=True)
+        for target in unigram_target:
+            temp = 0
+            try:
+                temp = unigram_bow[target]/unigram_count[index]
+            except:
+                temp = 0
+            resultRow.append(temp)
+        for target in bigram_target:
+            temp = 0
+            try:
+                temp = bigram_bow[target]/bigram_count[index]
+            except:
+                temp = 0
+            resultRow.append(temp)
+        for target in trigram_target:
+            temp = 0
+            try:
+                temp = trigram_bow[target]/trigram_count[index]
+            except:
+                temp = 0
+            resultRow.append(temp)
 
-    result.to_csv("BoW_Result_{0}.csv".format(brand))
+        result = result.append(pd.Series(resultRow, index=columns), ignore_index=True)
+
+    result.to_csv("Amazon_{0}_BoW_Result.csv".format(brand))
